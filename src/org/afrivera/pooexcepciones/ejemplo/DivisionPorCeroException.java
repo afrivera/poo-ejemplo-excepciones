132011b58895f4c54045ddeb649c1ee4c2e8bffb
afrivera <andresrfelip@hotmail.com>
2022-09-15 20:06:19 -0500

    add clases Calculadora y clase Exception para ejemplos

---
+++ src/org/afrivera/pooexcepciones/ejemplo/DivisionPorCeroException.java
@@ -0,0 +1,8 @@
+package org.afrivera.pooexcepciones.ejemplo;
+
+public class DivisionPorCeroException extends Exception{
+
+    public DivisionPorCeroException(String message) {
+        super(message);
+    }
+}
--- src/org/afrivera/pooexcepciones/ejemplo/EjemploExcepciones.java
+++ src/org/afrivera/pooexcepciones/ejemplo/EjemploExcepciones.java
@@ -1,9 +1,30 @@
 package org.afrivera.pooexcepciones.ejemplo;
 
+import javax.swing.*;
+
 public class EjemploExcepciones {
     public static void main(String[] args) {
 
-        int division = 10 / 0;
-        System.out.println(division);
+        Calculadora calc = new Calculadora();
+        String valor = JOptionPane.showInputDialog("Ingrese un numero");
+
+        int divisor;
+        double division;
+        try{
+            divisor = Integer.parseInt(valor);
+            division = calc.dividir(10,divisor);
+            System.out.println(division);
+
+        }catch (DivisionPorCeroException ae){
+            System.out.println("Capturamos la excepcion en timpo de ejecución " + ae.getMessage());
+            main(args);
+        } catch (NumberFormatException ne){
+            System.out.println("Segunda excepción Number format Exception" + ne.getMessage());
+            main(args);
+        } finally {
+            System.out.println("Esta acción siempre se ejecuta, por lo general para cerrar conexiones de base de datos o archivos abiertos");
+        }
+
+        System.out.println("continuamos el flujo de la aplicación");
     }
 }
