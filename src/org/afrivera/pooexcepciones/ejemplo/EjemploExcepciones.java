e46d220d6189b4008e8907811a38598038bd5d9c
afrivera <andresrfelip@hotmail.com>
2022-09-15 19:38:10 -0500

    commit initial exceptions

---
+++ src/org/afrivera/pooexcepciones/ejemplo/EjemploExcepciones.java
@@ -0,0 +1,9 @@
+package org.afrivera.pooexcepciones.ejemplo;
+
+public class EjemploExcepciones {
+    public static void main(String[] args) {
+
+        int division = 10 / 0;
+        System.out.println(division);
+    }
+}
